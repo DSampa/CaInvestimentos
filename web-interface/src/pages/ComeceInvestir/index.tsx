a9106479dd42c0100041284e6e33ab8940c0b561
DSampa <69985686+DSampa@users.noreply.github.com>
2020-10-07 18:56:28 -0300

    Front-end
    Mudança no front end das páginas "como investtir" e "sobre".

--- web-interface/src/pages/ComeceInvestir/index.tsx
+++ web-interface/src/pages/ComeceInvestir/index.tsx
@@ -1,45 +1,144 @@
 import React from 'react'
 import './styles.css'
+import LandingSideBar from '../../components/Institucional/Sidebar'
+import background from '../../assets/images/background-login.jpg'
+
+import imageChat from '../../assets/images/chat.jpeg'
+import imageNuvem from '../../assets/images/nuvens.jpeg'
+import imageConteudo from '../../assets/images/conteudo.jpg'
+import imageTransferenciaRecurso from '../../assets/images/tranferenciaRecurso.png'
+
 function ComeceInvestir(){
     return(
-    <div>
-        <fieldset id="container">
-            <legend><h2>Comece a Investir </h2></legend>
-            <div>
-                <h2>Abra sua Conta</h2>
-                <p>Para que possa ter multiplos benefinicios e fazer parte da melhor corretora desse Brasil abra a sua conta nesse exato momemnto de um modo simples e fácil clicando no botão abaixo!</p>
-                <input className="bot" type="button" name="button" value="ABRA JÁ SUA CONTA"></input>    
-            </div>
-            <img className="imagem" src="https://images.squarespace-cdn.com/content/v1/5c94eaaba9ab950b1b87a3f0/1564062112507-85E7JWI13P815EKPH6S0/ke17ZwdGBToddI8pDm48kFAExOXtrNfVtwFHrct4EAAUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKc5Anrr1fi3Hm8EHf_f3qXe9KbjTi1JJNGpXBWQEisknivHfFclH7VlfenNcw8oiUS/abrir+conta?format=750w" alt="imagem"></img>
-        </fieldset>
-            <fieldset id="container2"> 
-                <h2>1- Conteudo</h2>
-                <p className="text1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                <h2>2- Curadoria</h2>
-                <p className="text1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                <h2>3- Atendimento</h2>
-                <p className="text1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                <div className="text-left">
-                    <h2>4- Transferencia de Recursos</h2>
-                    <p className="text1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    <h3>Modalidade:</h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    <h3>Banco:</h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    <h3>Agencia:</h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    </div>
-            </fieldset>
-            <fieldset id="container3"> 
-                <h2>Transfencia de custodia</h2>
-                <p className="text1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                <h2>Vantagens de costudia na "CORRETORA"</h2>
-                <p className="text1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                <div className="text-left2">
-                    <h2>Ganhe com a gente</h2>
-                    <p className="text1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
-                    
-                    <img className="img" src="https://www.hostinger.com.br/tutoriais/wp-content/uploads/sites/12/2018/07/35-Maneiras-de-Como-Ganhar-Dinheiro-na-Internet-em-2019.png" alt="imagem-"></img>
-                    </div>
-            </fieldset>
+<body className="tag-body" >
+       
+     <div className="div-principal" >
+     
+    
+ {/*-------------*/}
+  {/*-------------*/}
+
+
+
+    
+
+  <LandingSideBar />
+        <header className="container-header">
+
+        <div className="landing-image-content" style={{ backgroundImage: `url(${background})` }}>
+        <img className="img" src="https://images.squarespace-cdn.com/content/v1/5c94eaaba9ab950b1b87a3f0/1564062112507-85E7JWI13P815EKPH6S0/ke17ZwdGBToddI8pDm48kFAExOXtrNfVtwFHrct4EAAUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKc5Anrr1fi3Hm8EHf_f3qXe9KbjTi1JJNGpXBWQEisknivHfFclH7VlfenNcw8oiUS/abrir+conta?format=750w" alt="imagem"></img>
+        <input className="bot" type="button" name="button" value="ABRA JÁ SUA CONTA"></input> 
+      
+        <h2 className="Comece-a-investir">Comece a Investir </h2>
+            <div className="box1">
+                <h2 className="cruzeiro-investimentos">Cruzeiro Investimentos</h2>
+                <p className="tag-p">Para que você possa ter multiplos benefício e fazer parte da melhor corretora desse Brasil. Abra a sua conta nesse exato momemnto de um modo simples rápido e fácil. Clicando no botão ao lado!</p>
+                 
+                 
+                </div>
+                
         </div>
+
+     
+         </header>
+         
+         {/*-------------------------------------------------------------------------------*/}
+
+  
+      <main className="conteiner-main">
+     <div className="conteudo">
+
+     <img src={imageConteudo} alt="" />
+     </div>
+      
+    
+      <div>
+
+      </div>
+      <div className="main-text1">
+      <h2>1- Conteudo:</h2>
+                <p className="text1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+                
+                <div className="nuvem">
+                <img src={imageNuvem} alt="" />
+                </div>
+ 
+<div className="main-text2">
+
+                <h2>2- Curadoria:</h2>
+                <p className="text1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+</div>
+
+<div className="chat">
+                <img src={imageChat} alt="" />
+                </div>
+
+<div className="main-text3">
+<h2>3- Atendimento:</h2>
+                <p className="text1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+               
+</div>
+
+<br />
+<div className="transferencia-recurso">
+
+     <img src={imageTransferenciaRecurso} alt="" />
+     </div>
+<div className="main-text4">
+<br />
+<h2>4- Transferencia de Recursos:</h2>
+                    <p className="text1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <h3 className="modalidade">Modalidade:</h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                    <h3 className="banco">Banco:</h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                    <h3 className="agencia">Agência:</h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                    </div>
+      </main>
+
+
+<aside className="conteiner-aside">
+
+<div>
+<img className="foto" src="https://www.hostinger.com.br/tutoriais/wp-content/uploads/sites/12/2018/07/35-Maneiras-de-Como-Ganhar-Dinheiro-na-Internet-em-2019.png" alt="imagem-"></img>
+
+<div className="aside1">
+
+<h2>Transfencia de custodia:</h2>
+                <p className="text1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+</div>
+
+
+<div className="aside2">
+
+<h2>Vantagens de costudia na "CORRETORA" &nbsp;:</h2>
+                <p className="text1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+</div>
+
+<div className="aside3">
+
+<h2>Ganhe com a gente:</h2>
+                    <p className="text1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>           
+                    
+</div>
+<div className="copi">
+&nbsp;  &nbsp;&#169; Outubro de 2020, Cruzeiro Investimentos.Todos os direitos reservados.
+</div>
+   
+
+</div>
+   
+ 
+</aside>
+
+
+ {/*-------------*/}
+  {/*-------------*/}
+
+
+
+    </div>
+    </body>
     )
 }
 export default ComeceInvestir
+
--- web-interface/src/pages/Sobre/index.tsx
+++ web-interface/src/pages/Sobre/index.tsx
@@ -1,66 +1,104 @@
 import React from 'react'
 import './styles.css'
+import LandingSideBar from '../../components/Institucional/Sidebar'
+import background from '../../assets/images/background-login.jpg'
+
+
 function Sobre() {
     return (
-        <body>
-            <div>
-                <fieldset id="container">
-                    <legend><h2>Introdução</h2></legend>
-                    <h2 className="texto" >Quem somos?</h2>
-                    <img className="image-p" src="http://www.pngall.com/wp-content/uploads/5/Money-Investment-PNG-Picture.png" alt="imagem-sobre"></img>
-                    <p className="texto-sobre"> A Cruzeiro Analytics é uma empresa de investimentos, cujo sonho é poder possibilitar para o máximo de pessoas de diferentes idades, regiões e nacionalidades um conhecimento avançado e amplo sobre o mercado financeiro e de como saber a melhor forma de investir de acordo com seus objetivo. A Cruzeiro Analyics além de banco tambem atua como uma corretorra, tendo assim todas as informações importantes sobre o mundo das finanças.</p>
-                </fieldset>
-                <div>
-                    <fieldset id="container">
-                        <legend><h2>Diferenciais em Comunicação e Suporte</h2></legend>
-                        <h2 className="texto1" >Duvidas? Suporte? Consultoria?</h2>
-                        <img className="image-suporte" src="https://i.pinimg.com/736x/69/1d/78/691d784832db9d221f4ec0ac9759da89.jpg" alt="imagem-suporte"></img>
-                        <p className="texto-sobre"> </p>
-                        <p>Nossa Corretora conta com um Canal de Atendimento para cada necessidade sua!</p>
-                        <p className="texto3">Na hora de conseguir resolver um problema, estruturar um processo ou lidar com objeções é preciso saber lidar com este momento. Diante disso, alguns princípios podem ajudar no bom atendimento ao cliente.
+        <div className="page-container">
+            <LandingSideBar />
+            <div className="page-content">
+                <div className="landing-image-content" style={{ backgroundImage: `url(${background})` }}>
+                    <div className="landing-content">
+                        <div className="landing-text-content">
+                            <h3 className="titulo">
+                                Quem somos?
+                            </h3>
+                            <h3> A Cruzeiro Analytics é uma empresa de investimentos, cujo sonho é poder possibilitar para o máximo de pessoas de diferentes idades, regiões e nacionalidades um conhecimento avançado e amplo sobre o mercado financeiro e de como saber a melhor forma de investir de acordo com seus objetivo. A Cruzeiro Analyics além de banco tambem atua como uma corretorra, tendo assim todas as informações importantes sobre o mundo das finanças.</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className="opcoes-text-content">
+                    <p className="titulo-black">
+                        Diferenciais em Comunicação e Suporte
+                    </p>
+                    <h4 className="titulo-black">
+                        Duvidas? Suporte? Consultoria?
+                    </h4>
+
+                    <img className="image-suporte" src="https://i.pinimg.com/736x/69/1d/78/691d784832db9d221f4ec0ac9759da89.jpg" alt="imagem-suporte"></img>
+                    <div className="comunicacao">
+                        <h4 className="costum-text">Nossa Corretora conta com um Canal de Atendimento para cada necessidade sua!</h4>
+                        <article className="textinho" >Na hora de conseguir resolver um problema, estruturar um processo ou lidar com
+                        objeções é preciso saber lidar com este momento. Diante disso, alguns princípios
+                        podem ajudar no bom atendimento ao cliente.
                         Comunicação: a comunicação é à base de tudo. Procure expor todas as informações de modo claro e objetivo.
                         Satisfação no atendimento: este ponto exige que todos os problemas ou dúvidas sejam convertidos em satisfação e fidelização.
                         Antipatia, jamais! O atendimento precisa se basear na simpatia e empatia do usuário
                         Outro de nosso diferencial é que nossas clientes terão acesso ao número particular de seu gerente ou consultor, para assim ter mais facilidades com dúvidas ou qualquer outro problema! Sua satisfação em ser bem atendido é nossa prioridade.
-</p>
 
-                    </fieldset>
-                    <fieldset id="container1">
-                        <legend><h2>Nosso Proposito</h2></legend>
-                        <h2 className="texto1" >Deixe seu Feedback no nosso Blog</h2>
-                        <a href="http://localhost:3000/blog"> Blog da Corretora</a>
-                        <h3 className="text-blog1">Informações sobre o blog</h3>
-                        <p className="texto-prop">No nosso blog você encontrará noticias, informações e dados sobre o mercado financeiro e ações para te auxiliar em decisões, ficar por dentro de tudo para que possa ter sua liberdade financeira.
-                         Em nosso você blog poderá criar uma conta em nosso corretora  de um modo simples e fácil
-                           </p>
-                        <img className="image-prop" src="https://s.clipartkey.com/mpngs/s/49-497739_investment-stock-market-clipart.png" alt="imagem-sobre"></img>
-                        <div className="text5">Não tem cadastro?
-                            <a href="http://localhost:3000/blog">Inscreva-se</a>
-                        </div>
-                    </fieldset>
-                    <fieldset id="container4">
-                    <legend><h2>Nossos números</h2></legend>
-                    <p>Central de Atendimento: 0800 15 100</p>
-                    <p>Suporte e ajuda: 4051-8725 </p>
-                    <p>Nossos atendimentos estão disponiveis por 24hr</p>
-                    <p>Para poder solucionar qualquer dúvida ou problema que tenha a qualquer hora! </p>
-                </fieldset>
-                <fieldset id="container5">Não perca tempo e 
-                    <a href="http://localhost:3000/blog">comece a investir</a>
-                    <p>Não perca essa grande oportunidade de mudar a sua vida e ter a grande e esperada liberdade financeira. Venha fazer parte do nosso time! estamos esperando por você!</p>
-               ,    <div>Já é membro? faça seu 
-                   <a href="http://localhost:3000/login">Login</a>
-                   <p>Ligue para algum de nossos números e tire suas dúvidas e venha para a melhor corretora desse Brasil! Venha fazer parte desse grande time!</p>
-               </div>
-                </fieldset>
-               
 
-                
+                        </article>
+                    </div>
                 </div>
+                <div className="informativo-text-content">
+                    
+                        <p className="titulo-proposito">
+                            Nosso Proposito
+                        </p>
+
+                        <div className="tex">
+                            <h3 className="titulo">Deixe seu Feedback no nosso Blog:</h3><br />
+                            <a className="bot-login" href="http://localhost:3000/blog"> Blog da Corretora</a><br /><br />
+                            <h3 className="text-blog1">Informações sobre o blog</h3>
+                                <article className="texto-informacao-blog" >No nosso blog você encontrará noticias, informações e dados sobre o mercado financeiro e ações para te auxiliar em decisões, ficar por dentro de tudo para que possa ter sua liberdade financeira.
+                            Em nosso você blog poderá criar uma conta em nosso corretora  de um modo simples e fácil.
+                            </article>
+                            <h3 className="pergunta-cadastro">Não tem cadastro?</h3><a className="bot-login" href="http://localhost:3000/login">Inscreva-se</a>
+                             
+                        </div>
+                        <img className="imag" src="https://s.clipartkey.com/mpngs/s/49-497739_investment-stock-market-clipart.png" alt="imagem-sobre"></img>        
+
+
+
+                        <div className="texto-comece-investir">
+                            <h2 className="titulo">Não perca tempo e : </h2>
+                            <h3><a href="http://localhost:3000/comece_investir">Comece a investir</a></h3>
+                            <p className="texto0">Não perca essa grande oportunidade de mudar a sua vida e ter a grande e esperada liberdade financeira. Venha fazer parte do nosso time! estamos esperando por você!</p>
+                        <div id="pergunta-membro">Já é membro? faça seu <h3><a href="http://localhost:3000/login">Login</a></h3>
+                                <p className="texto0">Ligue para algum de nossos números e tire suas dúvidas e venha para a melhor corretora desse Brasil! Venha fazer parte desse grande time!</p>
+                            </div><br />
+                            <h4 className="texto-numero">Nossos números:</h4><br /><br />
+                            <div className="numero-corretora">
+                            <p>Central de Atendimento: 0800 15 100</p>
+                                <p>Suporte e ajuda: 4051-8725 </p>
+                                <h3>Nossos atendimentos estão disponiveis por 24hr</h3>
+                                <h3>Para poder solucionar qualquer dúvida ou problema que tenha a qualquer hora! </h3>
+                                </div>
+                        </div>
+                               
+
+                        
+
+                    </div>
+                </div>
+
+
+
+
             </div>
 
 
-        </body>
+        
+
+
+
+
+
+
+
+
 
     )
 }
